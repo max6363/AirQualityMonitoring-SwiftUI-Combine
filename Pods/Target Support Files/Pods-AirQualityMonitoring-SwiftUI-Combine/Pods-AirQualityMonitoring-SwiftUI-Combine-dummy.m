e9dfaebd3c07261b681a5598c1112f561e131258
Minhaz Panara <minhaz.panara@gmail.com>
2021-12-20 16:32:26 +0530

    Add Pods to generate documentation.

---
+++ Pods/Target Support Files/Pods-AirQualityMonitoring-SwiftUI-Combine/Pods-AirQualityMonitoring-SwiftUI-Combine-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AirQualityMonitoring_SwiftUI_Combine : NSObject
+@end
+@implementation PodsDummy_Pods_AirQualityMonitoring_SwiftUI_Combine
+@end
